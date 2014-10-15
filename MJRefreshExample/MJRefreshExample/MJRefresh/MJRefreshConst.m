bd1faf794c77f33356c4c0cc64cd0f842673b56a
red3 <herui@Cross.local>
2014-10-15 11:06:41 +0800

    修改刷新提示文字

--- MJRefreshExample/MJRefreshExample/MJRefresh/MJRefreshConst.m
+++ MJRefreshExample/MJRefreshExample/MJRefresh/MJRefreshConst.m
@@ -14,13 +14,13 @@ const CGFloat MJRefreshSlowAnimationDuration = 0.4;
 
 NSString *const MJRefreshBundleName = @"MJRefresh.bundle";
 
-NSString *const MJRefreshFooterPullToRefresh = @"上拉可以加载更多数据";
-NSString *const MJRefreshFooterReleaseToRefresh = @"松开立即加载更多数据";
-NSString *const MJRefreshFooterRefreshing = @"MJ哥正在帮你加载数据...";
+NSString *const MJRefreshFooterPullToRefresh = @"上拉加载更多数据";
+NSString *const MJRefreshFooterReleaseToRefresh = @"放手刷新, 放手去爱";
+NSString *const MJRefreshFooterRefreshing = @"奋力加载, 奋力生活";
 
-NSString *const MJRefreshHeaderPullToRefresh = @"下拉可以刷新";
-NSString *const MJRefreshHeaderReleaseToRefresh = @"松开立即刷新";
-NSString *const MJRefreshHeaderRefreshing = @"MJ哥正在帮你刷新...";
+NSString *const MJRefreshHeaderPullToRefresh = @"下拉刷新";
+NSString *const MJRefreshHeaderReleaseToRefresh = @"放手刷新, 放手去爱";
+NSString *const MJRefreshHeaderRefreshing = @"奋力加载, 奋力生活";
 NSString *const MJRefreshHeaderTimeKey = @"MJRefreshHeaderView";
 
 NSString *const MJRefreshContentOffset = @"contentOffset";
